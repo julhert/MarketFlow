0df19aa904f8d18e0a1b50fffb5fbf4c2e494fae
Julian Trejo <ljht.2004@gmail.com>
2026-05-05 18:59:05 -0600

    feat(ui): implementar layout base y navegación responsiva

--- MarketFlow/routes/web.php
+++ MarketFlow/routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Jetstream\Jetstream;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Esta ruta (Catálogo) la pongo aquí porque necesitamos que no necesite 
+// autenticarse por Jetstream
+Route::get('/catalogo', function () {
+    return 'Vista del catálogo en construcción';})->name('catalogo');
 
 Route::middleware([
     'auth:sanctum',
