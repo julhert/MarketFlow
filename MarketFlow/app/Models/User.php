9f3da8923974377c074788363f89fa1925f15460
Julián Trejo <110367765+julhert@users.noreply.github.com>
2026-05-06 13:34:05 -0600

    refactor: reemplazar lógica de Jetstream Teams por directivas de Spatie (EC-61) (#16)
    Se removieron los permisos y roles de Jetstream Teams y se reemplazaron por los de Spatie. En el navigation-menu.blade.php
    
    Para que funcionara se tuvo que modificar archivos como User.php, agregar la librería del Spatie (spatie/laravel-permission) y hacer una migración para los permisos.
    
    Approved-by: Eueuito <lguebv@gmail.com>
    Co-authored-by: Julián Trejo <110367765+julhert@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Julián Trejo <110367765+julhert@users.noreply.github.com>
    Co-authored-by: NenucoDev <luisgomezm0987@gmail.com>
    Co-authored-by: Fernando Gómez <luisgomezm0987@mail.com>

--- MarketFlow/app/Models/User.php
+++ MarketFlow/app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -17,6 +18,7 @@ class User extends Authenticatable
 
     /** @use HasFactory<UserFactory> */
     use HasFactory;
+    use HasRoles;
 
     use HasProfilePhoto;
     use Notifiable;
