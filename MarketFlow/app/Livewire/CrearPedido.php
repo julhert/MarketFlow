780b48241ddfdb8c2c4ab38eef808bd5bebd2d41
Eueuito <lguebv@gmail.com>
2026-05-14 19:27:31 -0600

    no se que cambio, pero por si las moscas

--- MarketFlow/app/Livewire/CrearPedido.php
+++ MarketFlow/app/Livewire/CrearPedido.php
@@ -1,20 +1,16 @@
 <?php
 
-namespace App\Livewire\Pedidos;
+namespace App\Livewire;
 
 use App\Services\DireccionService;
+use App\Http\Requests\PedidoRequest;
+use App\Services\PedidoService;
 use App\Models\Carrito;
 use App\Models\Pedido;
 use Livewire\Component;
 
 class CrearPedido extends Component
 {
-    // Datos de prueba para maquetar
-    public $productos = [
-        ['nombre' => 'Objeto #1', 'cantidad' => 1, 'precio' => 1000],
-        ['nombre' => 'Objeto #2', 'cantidad' => 2, 'precio' => 2400],
-    ];
-
     public function render()
     {
         return view('livewire.pedidos.crear-pedido', [
@@ -38,7 +34,7 @@ class CrearPedido extends Component
     public function confirmarCompra(PedidoService $pedidoService)
     {
         // Validamos
-        $this->validate((new \App\Http\Requests\StorePedidoRequest())->rules());
+        $this->validate((new PedidoRequest())->rules());
 
         // Obtenemos los productos reales del carrito (no los de prueba)
         $items = Carrito::where('id_users', auth()->id())->with('producto')->get();
