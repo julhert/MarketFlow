be0056d9ba86d8ac2f57a735031c892bcad16e0f
Eueuito <lguebv@gmail.com>
2026-05-17 17:51:20 -0600

    fix: agregado a bootstrap, diciendo a laravel que confie en el proxy de railway

--- MarketFlow/bootstrap/app.php
+++ MarketFlow/bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
